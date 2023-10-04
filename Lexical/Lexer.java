347ad80ab9c78eac71a77c62908516bbb88ac4ce
Lee Zheng <leezheng1048@gmail.com>
2023-10-05 03:10:16 +0800

    processSlash /*****/ ungetChar

--- Lexical/Lexer.java
+++ Lexical/Lexer.java
@@ -176,6 +176,8 @@ public class Lexer {
                     next = getChar();
                     if (next == '/') {
                         break;
+                    } else {
+                        ungetChar();
                     }
                 }
             } while (true);
